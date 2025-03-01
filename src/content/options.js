cd726012ea528cc2b922f970f40382c7a848ba6c
AnsahMohammad <mohdansah10@gmail.com>
2025-03-01 12:06:26 +0530

    feat: add options for enable / disable of  transfer of pages and transfer of threads
    Signed-off-by: AnsahMohammad <mohdansah10@gmail.com>

--- src/content/options.js
+++ src/content/options.js
@@ -1,5 +1,7 @@
 const updateIntervalSlider = document.getElementById("update-interval-slider");
 const updateIntervalValue = document.getElementById("update-interval-value");
+const transferThreadsCheckbox = document.getElementById("transfer-threads");
+const transferWindowsCheckbox = document.getElementById("transfer-pages");
 
 function updateInterval(ev) {
   browser.runtime.sendMessage({
@@ -8,8 +10,25 @@ function updateInterval(ev) {
   })
 }
 
+function updateThreadsTransfer() {
+  browser.runtime.sendMessage({
+    name: "set-include-threads"
+  });
+  console.log("updateThreadsTransfer");
+}
+
+function updateWindowsTransfer() {
+  browser.runtime.sendMessage({
+    name: "set-include-windows"
+  });
+  console.log("updateWindowsTransfer");
+}
+
 updateIntervalSlider.addEventListener("change", updateInterval);
 updateIntervalSlider.addEventListener("input", ev => {
   updateIntervalValue.value = ev.target.value;
 });
 updateIntervalValue.value = updateIntervalSlider.value;
+
+transferThreadsCheckbox.addEventListener("change", updateThreadsTransfer);
+transferWindowsCheckbox.addEventListener("change", updateWindowsTransfer);
